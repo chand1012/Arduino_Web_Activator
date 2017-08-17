735ed8d27577ac891810de33af86e94c589d7ff2
chand1012 <chandlerl14@outlook.com>
2017-08-17 11:59:29 -0400

    Added lots of code

--- main_server/main_server.ino
+++ main_server/main_server.ino
@@ -11,14 +11,15 @@ const char* password = "chandnol911";
 
 ESP8266WebServer server(80);
 
-String webPage = "";
-
+//String webPage = "";
+String coffee1 = "";
+String coffee2 = "";
 int gpio0_pin = 0;
 int gpio2_pin = 2;
 
 void setup(void){
-  webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
-  webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
+  //webPage += "<h1>ESP8266 Web Server</h1><p>Socket #1 <a href=\"socket1On\"><button>ON</button></a>&nbsp;<a href=\"socket1Off\"><button>OFF</button></a></p>";
+  //webPage += "<p>Socket #2 <a href=\"socket2On\"><button>ON</button></a>&nbsp;<a href=\"socket2Off\"><button>OFF</button></a></p>";
 
   // preparing GPIOs
   pinMode(gpio0_pin, OUTPUT);
@@ -54,6 +55,7 @@ void setup(void){
     digitalWrite(gpio0_pin, HIGH);
     delay(1000);
   });
+  /*
   server.on("/socket1Off", [](){
     server.send(200, "text/html", webPage);
     digitalWrite(gpio0_pin, LOW);
@@ -69,6 +71,7 @@ void setup(void){
     digitalWrite(gpio2_pin, LOW);
     delay(1000);
   });
+  */
   server.begin();
   Serial.println("HTTP server started");
 }
